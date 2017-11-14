db3c8762196bed673ed771a3f6a128475cb6e518
Maximilien HEUZÉ <maximilien.heuze@sts-sio-caen.info>
2017-11-14 11:37:39 +0100

    mise en commun: page utilisateur

---
+++ homep/app/controllers/Connecte.php
@@ -0,0 +1,43 @@
+<?php
+namespace controllers;
+ use micro\orm\DAO;
+use Ajax\JsUtils;
+use Ajax\semantic\html\elements\HtmlButton;
+
+ /**
+ * Controller Connecte
+ * @property JsUtils $jquery
+ **/
+class Connecte extends ControllerBase{
+    
+    private $site;
+    private $utilisateur = 1;
+
+    public function initialize(){
+        parent::initialize();
+        $this->site=DAO::getOne("models\Site",0);
+    }
+    
+	/**
+	 * @route("/users")
+	 */
+	
+	public function index(){
+	    
+	    $liensPerso=DAO::getAll("models\Lienweb","idUtilisateur = ".$this->utilisateur);
+	    $liens=DAO::getAll("models\Lienweb");
+	    
+	    $semantic=$this->jquery->semantic();
+	    
+	    $bts=$this->jquery->semantic()->htmlButtonGroups("bts-liens");
+	    $bts->fromDatabaseObjects($liens, function($lien){
+	        $bt=new HtmlButton("",$lien->getLibelle(),"blue");
+	        $bt->asLink($lien->getUrl(),"_new");
+	        return $bt;
+	    });
+	    
+	    $this->jquery->compile($this->view);
+	    $this->loadView('Connecte.html');
+	    
+	}
+}
---
+++ homep/app/controllers/NonConnecte2.php
@@ -0,0 +1,44 @@
+<?php
+namespace controllers;
+use micro\orm\DAO;
+use Ajax\JsUtils;
+use Ajax\semantic\html\elements\HtmlButton;
+
+ /**
+ * Controller NonConnecte2
+ **/
+class NonConnecte2 extends ControllerBase{
+
+    private $site;
+    private $utilisateur = 1;
+    
+    public function initialize(){
+        parent::initialize();
+        $this->site=DAO::getOne("models\Site",0);
+    }
+    
+    /**
+     * @route("/users")
+     */
+    
+	public function index(){
+	    
+	    $liensPerso=DAO::getAll("models\Lienweb","idUtilisateur = ".$this->utilisateur);
+	    $liens=DAO::getAll("models\Lienweb");
+	    
+	    $semantic=$this->jquery->semantic();
+	  
+	    
+	    $bts=$this->jquery->semantic()->htmlButtonGroups("bts-liens");
+	    $bts->fromDatabaseObjects($liens, function($lien){
+	        $bt=new HtmlButton("",$lien->getLibelle(),"blue");
+	        $bt->asLink($lien->getUrl(),"_new");
+	        return $bt;
+	    });
+      
+	    
+	    $this->jquery->compile($this->view);
+	    $this->loadView('nonConnecte.html');
+
+	}
+}
